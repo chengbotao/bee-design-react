7a3e44ac493e5fc1a9bd503fbbe4e8f6f301ad8f
chengbotao <t221007s@163.com>
2020-11-05 15:50:19 +0800

    chore(rollup tsconfig babel tslint): add rollup ts tslint babel rules
    add rollup ts tslint babel rules

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,35 @@
  * @Author: Chengbotao
  * @Date: 2020-11-04 11:00:44
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-11-04 11:02:28
+ * @LastEditTime: 2020-11-05 15:41:35
  * @FilePath: \bee-design-react\rollup.config.js
  */
 
-export default {
-  
-}
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
+import tslint from "rollup-plugin-tslint";
+
+const options = {
+  input: 'src/index.tsx',
+  output: [{
+    file: path.resolve(__dirname, 'dist', 'bee.common.js'),
+    exports: "auto",
+    format: 'cjs'
+  }, {
+    file: path.resolve(__dirname, 'dist', 'bee.esm.js'),
+    format: 'es'
+  }, {
+    file: path.resolve(__dirname, 'dist', 'bee.js'),
+    format: 'umd',
+    name: "bee",
+    plugins: [terser()]
+  }],
+  external: ['react', 'react-dom'],
+  plugins: [resolve(), commonjs(), babel(), tslint(), typescript()]
+};
+
+export default options;
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Description: 
+ * @Author: Chengbotao
+ * @Date: 2020-11-05 14:24:57
+ * @LastEditors: Chengbotao
+ * @LastEditTime: 2020-11-05 14:58:14
+ * @FilePath: \bee-design-react\src\index.tsx
+ */
+
+import React from 'react';
+export default class ButtonComponent extends React.Component {
+  constructor(props: any) {
+    super(props)
+  }
+  render() {
+    return (
+      <button>ButtonComponent</button>
+    )
+  }
+}
