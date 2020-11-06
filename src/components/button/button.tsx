06519b00e1449d5635dbd5c6c6a3e93fdf14a482
chengbotao <t221007s@163.com>
2020-11-06 10:22:02 +0800

    feat(add button component): add button component
    add button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,64 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
+import classNames from "classnames";
+
+export type ButtonSize = "lg" | "sm";
+export type ButtonType = "primary" | "default" | "danger" | "link";
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+}
+
+/**
+ * @description: HTML button a 默认属性
+ * @param {type} 
+ * @return: 
+ */
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
+
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { className, disabled, size, btnType, children, style, ...restProps } = props;
+
+  // 添加 class: btn, [btn-lg, btn-primary]
+  const classes = classNames("btn", className, {
+    [`btn-${size}`]: size,
+    [`btn-${btnType}`]: btnType,
+    "disabled": (btnType === "link") && disabled
+  });
+
+  if (btnType === "link") {
+    return (
+      <a
+        className={classes}
+        style={style}
+        {...restProps}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        style={style}
+        disabled={disabled}
+        {...restProps}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: "default"
+}
+
+export default Button;
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,8 @@
  * @Author: Chengbotao
  * @Date: 2020-11-05 14:24:57
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-11-05 14:58:14
+ * @LastEditTime: 2020-11-06 09:54:59
  * @FilePath: \bee-design-react\src\index.tsx
  */
 
-import React from 'react';
-export default class ButtonComponent extends React.Component {
-  constructor(props: any) {
-    super(props)
-  }
-  render() {
-    return (
-      <button>ButtonComponent</button>
-    )
-  }
-}
+export { default as Button } from './components/button'
