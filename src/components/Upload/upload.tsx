b3ca9d90dc771a3e56ab3236d3ac98774382379e
chengbotao <t221007s@163.com>
2020-11-09 14:34:57 +0800

    feat(upload): add upload component

---
+++ src/components/Upload/upload.tsx
@@ -0,0 +1,176 @@
+import React, { FC, useRef, ChangeEvent, useState } from "react";
+import classNames from "classnames";
+import axios from "axios";
+import UploadList from "./uploadList";
+import Dragger from "../Dragger/dragger"
+
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error'
+export interface UploadFile {
+  uid: string;
+  size: number;
+  name: string;
+  status?: UploadFileStatus;
+  percent?: number;
+  raw?: File;
+  response?: any;
+  error?: any;
+}
+
+interface UploadProps {
+  className?: string;
+  action: string;
+  defaultFileList?: UploadFile[];
+  beforeUpload?: (file: File) => boolean | Promise<File>;
+  onProgress?: (percentage: number, file: UploadFile) => void;
+  onSuccess?: (data: any, file: UploadFile) => void;
+  onError?: (err: any, file: UploadFile) => void;
+  onChange?: (file: UploadFile) => void;
+  onRemove?: (file: UploadFile) => void;
+  headers?: { [key: string]: any };
+  name?: string;
+  data?: { [key: string]: any };
+  withCredentials?: boolean;
+  accept?: string;
+  multiple?: boolean;
+  drag?: boolean;
+}
+
+export const Upload: FC<UploadProps> = (props) => {
+  const {
+    action,
+    defaultFileList,
+    beforeUpload,
+    onProgress,
+    onSuccess,
+    onError,
+    onChange,
+    onRemove,
+    headers,
+    name,
+    data,
+    withCredentials,
+    accept,
+    multiple,
+    drag,
+    children,
+    className
+  } = props;
+
+  const classes = classNames("upload-component", className);
+  const fileInput = useRef<HTMLInputElement>(null);
+  const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || []);
+  const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
+    setFileList(prevList => {
+      return prevList.map(file => {
+        if (file.uid === updateFile.uid) {
+          return { ...file, ...updateObj }
+        } else {
+          return file
+        }
+      })
+    })
+  }
+  const handleClick = () => {
+    if (fileInput.current) {
+      fileInput.current.click()
+    }
+  }
+  const handleRemove = (file: UploadFile) => {
+    setFileList((prevList) => {
+      return prevList.filter(item => item.uid !== file.uid)
+    })
+    onRemove && onRemove(file)
+  }
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) {
+      return;
+    }
+    uploadFiles(files);
+    if (fileInput.current) {
+      fileInput.current.value = ""
+    }
+  }
+
+
+
+  const uploadFiles = (files: FileList) => {
+    let postFiles = Array.from(files);
+    postFiles.forEach(file => {
+      if (!beforeUpload) {
+        post(file);
+      } else {
+        const result = beforeUpload(file)
+        if (result && result instanceof Promise) {
+          result.then((processedFile) => {
+            post(processedFile)
+          })
+        } else if (result !== false) {
+          post(file)
+        }
+      }
+
+    })
+  }
+
+  const post = (file: File) => {
+    let _file: UploadFile = {
+      uid: `${Date.now()}upload-file`,
+      status: "ready",
+      name: file.name,
+      size: file.size,
+      percent: 0,
+      raw: file
+    }
+    setFileList(prevList => {
+      return [_file, ...prevList]
+    })
+    const formData = new FormData();
+    formData.append(name!, file);
+    if (data) {
+      Object.keys(data).forEach(key => {
+        formData.append(key, data[key])
+      })
+    }
+    axios.post(action, formData, {
+      headers: {
+        ...headers,
+        "Content-Type": "multipart/from-data"
+      },
+      withCredentials,
+      onUploadProgress: (e: any) => {
+        let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+        if (percentage < 100) {
+          updateFileList(_file, { percent: percentage, status: "uploading" })
+          onProgress && onProgress(percentage, _file)
+        }
+      }
+    }).then((resp: any) => {
+      updateFileList(_file, { status: "success", response: resp.data })
+      onSuccess && onSuccess(resp.data, _file)
+      onChange && onChange(_file);
+    }).catch((err: Error) => {
+      updateFileList(_file, { status: "error", error: err })
+      onError && onError(err, _file)
+      onChange && onChange(_file);
+    })
+  }
+
+  return (
+    <div className={classes}>
+      <div className="upload-input" style={{ display: "inline-block" }} onClick={handleClick}>
+        {drag ? <Dragger onFile={(files: FileList) => { uploadFiles(files) }}>
+          {children}
+        </Dragger> : children}
+        <input type="file" className="file-input" ref={fileInput} style={{ display: 'none' }} onChange={handleFileChange} accept={accept} multiple={multiple} />
+      </div>
+      <UploadList fileList={fileList} onRemove={handleRemove}></UploadList>
+    </div>
+  )
+}
+
+Upload.defaultProps = {
+  name: "file"
+}
+
+export default Upload;
