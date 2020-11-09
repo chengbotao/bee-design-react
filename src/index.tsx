e6fb0b4d5f2a9f14768d77dfad5e7cb8e470443b
chengbotao <t221007s@163.com>
2020-11-09 14:46:44 +0800

    fix(index.tsx): fix index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@
  * @Author: Chengbotao
  * @Date: 2020-11-05 14:24:57
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-11-06 09:54:59
+ * @LastEditTime: 2020-11-09 14:43:08
  * @FilePath: \bee-design-react\src\index.tsx
  */
 
-export { default as Button } from './components/button'
+export { default as Button } from './components/Button'
