cab87b1970ac7399cf2fdcc21b94760f33eccd77
chengbotao <chengbotao5221@163.com>
2020-11-22 11:22:33 +0800

    docs(menu): add menu test

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,7 +2,7 @@
  * @Author: Chengbotao
  * @Description: 
  * @Date: 2020-11-07 22:30:17
- * @LastEditTime: 2020-11-19 00:05:55
+ * @LastEditTime: 2020-11-22 09:46:27
  * @LastEditors: Chengbotao
  * @FilePath: \bee-design-react\.storybook\preview.tsx
  */
@@ -10,7 +10,9 @@
 import React from 'react';
 import { addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 const wrapperStyle: React.CSSProperties = {
   padding: '20px 40px'
 }
